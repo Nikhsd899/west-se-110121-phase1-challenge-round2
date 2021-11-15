8df73da72509a2026a6bb7a466e2e4d8920cecb9
Nicholas <nikhsd899@outlook.com>
2021-11-15 11:50:44 -0800

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("you got this!");
+const url = "https://distinct-vaulted-freesia.glitch.me/image"
+const cardImg = document.getElementById
+const cardTitle = document.getElementById
+const commentList = document.getElementById
+ 
+function (photo){
+    cardTitle.innerText = photo.title
+    commentList.innerHTML =
+    ${photo.comments[0].content}</li>
+    ${photo.comments[1].content}</li
+    ${photo.comments[2].content}</li>`
+    likeBtn.addEventListener("click", addLikes)
+ 
+ 
+commentForm.addEventListener("submit", postComment)
+function postComment(e){
+    e.preventDefault()
+    let newComment = document.createElement("li")
+ 
+    commentList.append(newComment)
+   
