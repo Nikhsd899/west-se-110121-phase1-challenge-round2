89d37606bb0d2157e38433128c5b2408548a4850
Nicholas <nikhsd899@outlook.com>
2021-11-15 15:46:24 -0800

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 const url = "https://distinct-vaulted-freesia.glitch.me/image"
-const cardImg = document.getElementById
-const cardTitle = document.getElementById
-const commentList = document.getElementById
- 
-function (photo){
+const cardImg = document.getElementById("fg-image")
+const cardTitle = document.getElementById("fg-title")
+const commentForm = document.getElementById("comment-form")
+const commentList = document.getElementById("fg-comments")
+const likeBtn = document.getElementById("like-button")
+const likes = document.getElementById("fg-likes")
+
+fetch(url)
+.then(res => res.json())
+.then(renderPhoto)
+
+function renderPhoto(photo){
     cardTitle.innerText = photo.title
-    commentList.innerHTML =
-    ${photo.comments[0].content}</li>
-    ${photo.comments[1].content}</li
-    ${photo.comments[2].content}</li>`
+    cardImg.src = photo.image
+    commentList.innerHTML = 
+    `<li>${photo.comments[0].content}</li>
+    <li>${photo.comments[1].content}</li
+    <li>${photo.comments[2].content}</li>`
     likeBtn.addEventListener("click", addLikes)
- 
- 
+
+ function addLikes() {
+    likes.innerText = +photo.likes + "Likes"
+}
+
 commentForm.addEventListener("submit", postComment)
-function postComment(e){
+function postComment(e) {
     e.preventDefault()
     let newComment = document.createElement("li")
- 
-    commentList.append(newComment)
-   
+    newComment.textContent = e.target.comment.value
+    commentList.append(newComment)   
+
+    }
+}
